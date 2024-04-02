65be1e174818fc7fee8cac8693ffcbd31d023d3f
HAFIZ SUFYAN Khan <161612341+sufyan2465@users.noreply.github.com>
2024-04-02 16:21:51 +0500

    push my persona message project on github

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Hello Eric,
+//“He
+//would you like to learn some Python today?”
+let firstName = "Eric";
+console.log(`Hello ${firstName} ,“would you like to learn some Python today?”`);
---
+++ main.ts
@@ -0,0 +1,2 @@
+let firstName: String = "Eric";
+console.log(`Hello ${firstName} ,“would you like to learn some Python today?”`);
